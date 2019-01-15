4c76ecc2271a88d69c8fcb3d4a3b7794b14e7112
Christophe Eymard <christophe.eymard@sales-way.com>
2019-01-15 15:44:19 +0100

    ...

--- rc.lua
+++ rc.lua
@@ -356,49 +356,47 @@ keys.global('XF86AudioPlay', sendToSpotify('PlayPause'))
                 {description = "show the menubar", group = "launcher"})
 
 
-clientkeys = gears.table.join(
-    awful.key({ modkey,           }, "f",
-        function (c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end,
-        {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
-              {description = "close", group = "client"}),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
-              {description = "toggle floating", group = "client"}),
-    awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
-              {description = "move to master", group = "client"}),
-    awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
-              {description = "move to screen", group = "client"}),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-              {description = "toggle keep on top", group = "client"}),
-    awful.key({ modkey,           }, "n",
-        function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end ,
-        {description = "minimize", group = "client"}),
-    awful.key({ modkey,           }, "m",
-        function (c)
-            c.maximized = not c.maximized
-            c:raise()
-        end ,
-        {description = "(un)maximize", group = "client"}),
-    awful.key({ modkey, "Control" }, "m",
-        function (c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end ,
-        {description = "(un)maximize vertically", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "m",
-        function (c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end ,
-        {description = "(un)maximize horizontally", group = "client"})
-)
+.client('Win f',
+                function (c)
+                    c.fullscreen = not c.fullscreen
+                    c:raise()
+                end,
+                {description = "toggle fullscreen", group = "client"})
+.client('Win Shift c', function (c) c:kill() end,
+                {description = "close", group = "client"})
+.client('Win Ctrl space',  awful.client.floating.toggle                     ,
+                {description = "toggle floating", group = "client"})
+.client('Win Ctrl Return', function (c) c:swap(awful.client.getmaster()) end,
+                {description = "move to master", group = "client"})
+.client('Win o', function (c) c:move_to_screen() end,
+                {description = "move to screen", group = "client"})
+.client('Win t', function (c) c.ontop = not c.ontop end,
+                {description = "toggle keep on top", group = "client"})
+.client('Win n',
+                function (c)
+                    -- The client currently has the input focus, so it cannot be
+                    -- minimized, since minimized clients can't have the focus.
+                    c.minimized = true
+                end ,
+                {description = "minimize", group = "client"})
+.client('Win m',
+    function (c)
+        c.maximized = not c.maximized
+        c:raise()
+    end ,
+    {description = "(un)maximize", group = "client"})
+.client('Win Ctrl m',
+    function (c)
+        c.maximized_vertical = not c.maximized_vertical
+        c:raise()
+    end ,
+    {description = "(un)maximize vertically", group = "client"})
+.client('Win Shift m',
+    function (c)
+        c.maximized_horizontal = not c.maximized_horizontal
+        c:raise()
+    end ,
+    {description = "(un)maximize horizontally", group = "client"})
 
 
 -- Bind all key numbers to tags.
@@ -513,7 +511,7 @@ awful.rules.rules = {
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      raise = true,
-                     keys = clientkeys,
+                     keys = keys.clientkeys,
                      buttons = clientbuttons,
                      screen = awful.screen.preferred,
                      placement = awful.placement.no_overlap+awful.placement.no_offscreen
